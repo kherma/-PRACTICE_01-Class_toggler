7d3923c1d9fbf77696892cf734f8413217b9d47e
kherma <kamil.herma01@gmail.com>
2020-10-28 02:03:20 +0100

    Add event listener to selector function

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const modalWindow = document.querySelector('.modal');
 const overlayBackground = document.querySelector('.overlay');
 const btnShowModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
+
+// Add event listener to arg
+function addListenerToggler (btn) {
+    btn.addEventListener('click', function (){
+        modalWindow.classList.toggle('hidden');
+        overlayBackground.classList.toggle('hidden');
+    });
+}
+
+
+for (let showModal of btnShowModal) {
+    addListenerToggler(showModal);
+}
+addListenerToggler(btnCloseModal);
+addListenerToggler(overlayBackground);
