170a058429879788c373de8445089a8ccaf2ca03
kherma <kamil.herma01@gmail.com>
2020-10-28 02:03:20 +0100

    Add class toggler function

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ const overlayBackground = document.querySelector('.overlay');
 const btnShowModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 
-for (let showModal of btnShowModal) {
-    showModal.addEventListener('click', function(){
-        modalWindow.classList.remove('hidden');
-        overlayBackground.classList.remove('hidden');
+// Add event listener to arg
+function hiddenToggler (btn) {
+    btn.addEventListener('click', function (){
+        modalWindow.classList.toggle('hidden');
+        overlayBackground.classList.toggle('hidden');
     });
 }
+
+
+for (let showModal of btnShowModal) {
+    hiddenToggler(showModal);
+}
+hiddenToggler(btnCloseModal);
+hiddenToggler(overlayBackground);
