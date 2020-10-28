51c5a9a98b94265473cf35d9802e74f5b2b8b3bc
Kamil Herma <63641476+kherma@users.noreply.github.com>
2020-10-28 03:03:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const overlayBackground = document.querySelector('.overlay');
 const btnShowModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 
-// Add event listener to arg
+// Add event listener to argument
 function addListenerToggler (btn) {
     btn.addEventListener('click', function (){
         modalWindow.classList.toggle('hidden');
@@ -13,9 +13,16 @@ function addListenerToggler (btn) {
     });
 }
 
-
+// Add class toggler to buttons
 for (let showModal of btnShowModal) {
     addListenerToggler(showModal);
 }
 addListenerToggler(btnCloseModal);
 addListenerToggler(overlayBackground);
+
+// Add class toggler to 'esc' key
+document.addEventListener('keydown', function(event){
+    if(!modalWindow.classList.contains('hidden') && event.keyCode == 27) {
+        classToggler();
+    }
+});
