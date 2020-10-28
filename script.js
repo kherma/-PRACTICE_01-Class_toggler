e1b20ea5decbb8a0e602d8019af21a99bf1709f1
kherma <kamil.herma01@gmail.com>
2020-10-28 01:41:20 +0100

    Declare variables

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const modalWindow = document.querySelector('.modal');
+const overlayBackground = document.querySelector('.overlay');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
