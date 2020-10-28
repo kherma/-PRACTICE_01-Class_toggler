b5e6fe45926376801ae42e1bc9efaf4973647673
kherma <kamil.herma01@gmail.com>
2020-10-28 01:42:36 +0100

    Add listener for each show-modal button

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const modalWindow = document.querySelector('.modal');
 const overlayBackground = document.querySelector('.overlay');
 const btnShowModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
+
+for (let showModal of btnShowModal) {
+    showModal.addEventListener('click', function(){
+        console.log(showModal.textContent);
+    });
+}
